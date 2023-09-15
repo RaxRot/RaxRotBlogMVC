0de6cba2c271b552c748eb0c4572b22abb1734da
Vladosss <dasistperfektosss@gmail.com>
2023-09-15 18:11:35 +0200

    Add project files.

---
+++ RaxRot.Blog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RaxRot.Blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
